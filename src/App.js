432fe70d2069e09f6d3743c6bc6d5128e614ebe3
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-04-04 15:50:45 -0400

    created add new entry property component in the App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
-
+import AddEntryForm from "./Components/AddEntry/AddEntryForm";
 
 function App() {
   const [entries, setEntries] = useState([
     {weight: 175, date: "11-23-2021"}, {weight: 176, date: "11-24-2021"}
   ]);
+  
+  function addNewEntry(entry){
+    let tempEntries = [entry, ...entries];
+    setEntries(tempEntries)
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries}/>
+      <AddEntryForm addNewEntryProperty={addNewEntry}/>
     </div>
   );
 }
---
+++ src/Components/AddEntry/AddEntryForm.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+
+
+const AddEntryForm = (props) => {
+    
+    
+    const [weight, setWeight] = useState(0);
+    const [date, setDate] = useState("");
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newEntry = {
+            weight: weight,
+            date: date
+        }
+        console.log(newEntry);
+        props.addNewEntryProperty(newEntry)
+    }
+
+
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <lable>Weight</lable>
+            <input type="number" value={weight} onChange={(event) => setWeight(event.target.value) }/>
+            <lable>Date</lable>
+            <input type="date" value={date} onChange={(event) => setDate(event.target.value) }/>
+            <button type="submit">Add</button>
+        </form>
+     );
+}
+
+export default AddEntryForm;
