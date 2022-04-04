c779ee7bd756fa9d9b26503a64b35bd18a1b6c0c
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-04-04 15:23:32 -0400

    created display entries component and instantiated component on App component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
+
+
+function App() {
+  const [entries, setEntries] = useState([
+    {weight: 175, date: "11-23-2021"}, {weight: 176, date: "11-24-2021"}
+  ]);
+  return (
+    <div>
+      <DisplayEntries parentEntries={entries}/>
+    </div>
+  );
+}
+
+export default App;
